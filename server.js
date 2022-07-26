d178d6eb8d2ad275e673d3c16654a650a1e5d895
Sara <sameads9@gmail.com>
2022-07-26 13:12:44 -0500

    build database call

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
+// connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Cod1ng!!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // default response for any other request (Not Found)
